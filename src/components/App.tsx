cfd360efe463c592421a4e70f5bcd1943ba40ee6
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-17 19:13:05 +0300

    start project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { theme } from '../src/theme/theme';
+import { theme } from '../theme/theme';
 
 function App() {
   return (
