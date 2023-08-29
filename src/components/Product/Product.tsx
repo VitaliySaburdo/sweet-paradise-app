3d35f6690553d5ea111eccd304317627f6fa9cbe
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-29 11:04:10 +0300

    fix txt

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -24,7 +24,7 @@ export const Product: React.FC<NoveltiesItem> = ({ product }) => {
               <Img src={"https://sweet-paradise-api.onrender.com/static/" + product.img} alt={product.name}/>
               <Title>{product.name}</Title>
               <Text>{product.ingredients}</Text>
-              <Params>{product.price} uah /{product.weight} gr</Params>
+              <Params>{product.price} uah / {product.weight} gr</Params>
               <Button width="200px">add to cart</Button>
       </Wrapper>
     </>
