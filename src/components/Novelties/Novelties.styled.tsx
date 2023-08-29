21d1c84a9752ae25e79d55128f53072a2e2fbc25
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-29 11:02:02 +0300

    fix

--- src/components/Novelties/Novelties.styled.tsx
+++ src/components/Novelties/Novelties.styled.tsx
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
   color: #444251;
   text-align: center;
   font-family: Montserrat;
-  font-size: 44px;
+  font-size: 24px;
   font-style: normal;
   font-weight: 900;
   line-height: normal;
-  margin-bottom: 79px;
+  margin-bottom: 60px;
+  @media screen and (${(props) => props.theme.media.lg}) {
+    width: 100%;
+    font-size: 44px;
+    margin-bottom: 79px;
+  }
 `;
 
 export const Wrapper = styled.ul`
