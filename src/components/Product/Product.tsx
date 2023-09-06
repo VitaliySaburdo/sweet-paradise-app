2aa21c2e98fb81a52af115ad38766af2bdc751d0
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-06 22:08:42 +0300

    add

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,17 +4,18 @@ import { Img, Wrapper, Title, Text, Params } from "../Product/Products.styled";
 import { Button } from "../Button/Button";
 import { Modal } from "../Modal/Modal";
 import { ProductProps } from "../App/App.types";
-import { CartItem } from "../CartItem/CartItem";
-
+import { CartList } from "../CartList/CartList";
 
 export const Product: React.FC<ProductProps> = ({ product }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [cart, setCart] = useState<ProductProps[]>([]);
 
-
-  const handleButtonClick = () => {
+  const handleButtonClick = ({product}: any) => {
     setIsModalOpen(true);
+    setCart([...cart, product]);
   };
 
+  console.log(cart);
 
   const closeModal = () => {
     setIsModalOpen(false);
@@ -32,16 +33,13 @@ export const Product: React.FC<ProductProps> = ({ product }) => {
         <Params>
           {product.price} uah / {product.weight} gr
         </Params>
-        <Button
-          width="200px"
-          onClick={handleButtonClick}
-        >
+        <Button width="200px" onClick={()=>handleButtonClick({product})}>
           to buy
         </Button>
       </Wrapper>
       {isModalOpen && (
         <Modal onClick={closeModal}>
-          <CartItem product={product} />
+          <CartList cart={cart} />
         </Modal>
       )}
     </>
