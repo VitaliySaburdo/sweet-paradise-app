84bdd17c52e52f197d50965ce60cba01d2cda22e
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-11 22:23:36 +0300

    add catalog

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -7,6 +7,28 @@ export const Catalog = () => {
       <Container>
         <Wrapper>
           <h1>Yummy catalog</h1>
+        <ul>
+          <li>
+            <img src="" alt="Eclair" />
+            <p>Eclair</p>
+          </li>
+          <li>
+            <img src="" alt="Macaron" />
+            <p>Macaron</p>
+          </li>
+          <li>
+            <img src="" alt="Cupcake" />
+            <p>Cupcake</p>
+          </li>
+          <li>
+            <img src="" alt="Muffin" />
+            <p>Muffin</p>
+          </li>
+          <li>
+            <img src="" alt="Donut" />
+            <p>Donut</p>
+          </li>
+        </ul>
         </Wrapper>
       </Container>
     </>
