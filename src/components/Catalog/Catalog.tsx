29883a48c29e346e1f034cb11c358eb4d6f2d97d
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-12 11:20:49 +0300

    add img

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,37 +1,42 @@
 import { Container } from "../Container/Container";
 import { Section } from "../Section/Section";
 import { CatalogList, CatalogTitle } from "./Catalog.styled";
+import eclair from "../../images/Goods/eclair.png";
+import macaron from "../../images/Goods/macaron.png";
+import cupcake from "../../images/Goods/cupcake.png";
+import muffin from "../../images/Goods/muffin.png";
+import donut from "../../images/Goods/donut.png";
 
 export const Catalog = () => {
   return (
     <>
       <Section>
-      <Container>
+        <Container>
           <CatalogTitle>Yummy catalog</CatalogTitle>
-        <CatalogList>
-          <li>
-            <img src="" alt="Eclair" />
-            <p>Eclair</p>
-          </li>
-          <li>
-            <img src="" alt="Macaron" />
-            <p>Macaron</p>
-          </li>
-          <li>
-            <img src="" alt="Cupcake" />
-            <p>Cupcake</p>
-          </li>
-          <li>
-            <img src="" alt="Muffin" />
-            <p>Muffin</p>
-          </li>
-          <li>
-            <img src="" alt="Donut" />
-            <p>Donut</p>
-          </li>
-        </CatalogList>
+          <CatalogList>
+            <li>
+              <img src={eclair} alt="Eclair" />
+              <p>Eclair</p>
+            </li>
+            <li>
+              <img src={macaron} alt="Macaron" />
+              <p>Macaron</p>
+            </li>
+            <li>
+              <img src={cupcake} alt="Cupcake" />
+              <p>Cupcake</p>
+            </li>
+            <li>
+              <img src={muffin} alt="Muffin" />
+              <p>Muffin</p>
+            </li>
+            <li>
+              <img src={donut} alt="Donut" />
+              <p>Donut</p>
+            </li>
+          </CatalogList>
         </Container>
-        </Section>
+      </Section>
     </>
   );
 };
