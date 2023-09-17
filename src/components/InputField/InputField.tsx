f4fe2639e46153015c918407a6bf2a8e05ae7c6b
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-17 18:24:59 +0300

    input

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,3 +1,21 @@
-export const InputField = () => {
-    return <></>
+import {StyledField} from './InputField.styled';
+interface InputProps {
+    type: string,
+    id: string,
+    name: string,
+    placeholder: string
 }
+
+export const InputField: React.FC<InputProps> = ({ type, id, name, placeholder }) => {
+  return (
+    <>
+      {" "}
+      <StyledField
+        type={type}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
