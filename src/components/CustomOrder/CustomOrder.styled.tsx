e6ffdf4a614d5a703cc02a8cdd049b98fb76586d
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-24 20:56:40 +0300

    md

--- src/components/CustomOrder/CustomOrder.styled.tsx
+++ src/components/CustomOrder/CustomOrder.styled.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import { Section } from "../Section/Section";
 import box from "../../images/CustomOrder/box.png";
-import leaf from '../../images/CustomOrder/leaf.png';
-import leaf_blur from '../../images/CustomOrder/leaf_blur.png';
+// import leaf from '../../images/CustomOrder/leaf.png';
+// import leaf_blur from '../../images/CustomOrder/leaf_blur.png';
 
 
 export const StyledSection = styled(Section)`
   border: 1px solid #000;
   padding-top: 0;
   padding-bottom: 0;
+    @media screen and (${(props) => props.theme.media.md}) {
+    background-image:  
+      linear-gradient(97deg, #fca4c9 -7.93%, #fe71ad 55.51%, #b44e79 104.2%);
+    /* background-position: 80% 100%; */
+    background-repeat: no-repeat;
+  }
   @media screen and (${(props) => props.theme.media.lg}) {
     background-image:  url(${box}),
       linear-gradient(97deg, #fca4c9 -7.93%, #fe71ad 55.51%, #b44e79 104.2%);
@@ -21,6 +27,10 @@ export const Box = styled.div`
   margin-top: 180px;
   margin-bottom: 190px;
   width: 500px;
+  @media screen and (${(props) => props.theme.media.md}){
+    margin-left: auto;
+    margin-right: auto;
+  }
 `;
 
 export const Title = styled.h2`
@@ -31,6 +41,9 @@ export const Title = styled.h2`
   font-weight: 900;
   line-height: normal;
   margin-bottom: 40px;
+  @media screen and (${(props) => props.theme.media.md}){
+    text-align: center;
+  }
 `;
 
 export const AccentText = styled.span`
