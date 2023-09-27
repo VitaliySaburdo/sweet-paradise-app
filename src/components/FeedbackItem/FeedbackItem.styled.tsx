1537af739db5b49f358c87f5b712ef8b1c0c0717
VitaliySaburdo <vsaburdo@gmail.com>
2023-09-27 10:51:15 +0300

    fix feedback

--- src/components/FeedbackItem/FeedbackItem.styled.tsx
+++ src/components/FeedbackItem/FeedbackItem.styled.tsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.li`
-  min-width: 800px;
+  min-width: 300px;
   padding: 20px;
   border-radius: 5px;
   border: 1px solid #ffd4dd;
-  box-shadow:  4px 4px 10px 1px rgba(226, 226, 226, 0.46);
+  box-shadow: 4px 4px 10px 1px rgba(226, 226, 226, 0.46);
+  @media screen and (${(props) => props.theme.media.md}) {
+    min-width: 700px;
+  }
+  @media screen and (${(props) => props.theme.media.lg}) {
+    min-width: 800px;
+  }
 `;
 export const Box = styled.div`
   display: flex;
