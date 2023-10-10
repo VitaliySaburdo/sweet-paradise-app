22e6d31fdda098434a9b64bfaf6bf4e73895482c
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-10 21:34:07 +0300

    add scroll link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,25 +1,28 @@
 import { Nav, NavList, StyledLink} from "./NavBar.styled";
 
-// import user from "../../images/Header/user.png";
-
 interface NavBarProps {
   color?: string;
   display?: string;
 }
 
-export const NavBar: React.FC<NavBarProps> = ({color, display = false}) => {
+export const NavBar: React.FC<NavBarProps> = ({ color, display = false }) => {
+  
+  const handleOnClick = () => {
+    window.scrollTo(0, 0);
+}
+
   return (
     <>
       <Nav>
         <NavList display={display.toString()}>
           <li>
-             <StyledLink to={'/'} color={color} >Home</StyledLink>
+             <StyledLink onClick={handleOnClick} to={'/'} color={color} >Home</StyledLink>
           </li>
           <li>
-            <StyledLink to={'/goods'} color={color} >Catalog</StyledLink>
+            <StyledLink onClick={handleOnClick} to={'/goods'} color={color} >Catalog</StyledLink>
           </li>
           <li>
-            <StyledLink to={'/feedback'} color={color} >Feedback</StyledLink>
+            <StyledLink onClick={handleOnClick} to={'/feedback'} color={color} >Feedback</StyledLink>
           </li>
           <li>
             <p color={color}>Contacts</p>
