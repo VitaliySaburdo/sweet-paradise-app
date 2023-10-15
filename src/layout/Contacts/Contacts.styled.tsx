a38e53269cd1e0af48b5040caa2ded70b34c2c96
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-15 19:07:27 +0300

    adress

--- src/layout/Contacts/Contacts.styled.tsx
+++ src/layout/Contacts/Contacts.styled.tsx
@@ -4,12 +4,14 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     margin-top: 20px;
     display: flex;
+    column-gap: 20px;
+    margin-bottom: 20px;
 `
 
 export const MainTitle = styled.h2`
     margin-bottom: 20px;
 `
-export const Title = styled.h2`
+export const Title = styled.h3`
     margin-bottom: 20px;
 `
 export const PhoneContainer = styled.ul`
