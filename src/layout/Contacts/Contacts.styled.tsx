ba2150f5cf115a4ed682c82a512b225cdb59dc0f
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-15 18:32:58 +0300

    add

--- src/layout/Contacts/Contacts.styled.tsx
+++ src/layout/Contacts/Contacts.styled.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
+
 export const Wrapper = styled.div`
     margin-top: 20px;
+    display: flex;
 `
 
 export const MainTitle = styled.h2`
@@ -9,4 +11,9 @@ export const MainTitle = styled.h2`
 `
 export const Title = styled.h2`
     margin-bottom: 20px;
+`
+export const PhoneContainer = styled.ul`
+    display: grid;
+    grid-template-columns: [first] 220px [line2] 220px;
+    row-gap: 20px;
 `
--- src/layout/Contacts/Contacts.tsx
+++ src/layout/Contacts/Contacts.tsx
@@ -1,68 +1,74 @@
 import { Link } from "react-router-dom";
 import { Container } from "../../components/Container/Container";
 import { Section } from "../../components/Section/Section";
-import {MainTitle, Wrapper, Title} from './Contacts.styled';
+import { MainTitle, Wrapper, Title, PhoneContainer } from "./Contacts.styled";
 
 export const Contacts = () => {
   return (
     <>
       <Section>
         <Container>
+          <MainTitle>Contacts</MainTitle>
           <Wrapper>
-            <MainTitle>Contacts</MainTitle>
-            <Title>Call us</Title>
-            <ul>
-              <li>
-                <Link to="tel:0800222222">0 (800) 22-22-22</Link>
-                <span>Free calls</span>
-              </li>
-              <li>
-                <Link to="tel:+380673333333">+380 (67) 333-33-33</Link>
-                <span>Kyiv</span>
-              </li>
-              <li>
-                <Link to="tel:+380503333333">+380 (50) 333-33-33</Link>
-                <span>Lviv</span>
-              </li>
-              <p>Write us</p>
-            </ul>
-            <ul>
-              <li>
-                <Link to="https://www.viber.com/ua/">Viber</Link>
-              </li>
-              <li>
-                <Link to="https://web.telegram.org/">Telegram</Link>
-              </li>
-              <li>
-                <Link to="mailto:sweet_paradise@mail.com">
-                  sweet_paradise@mail.com
-                </Link>
-              </li>
-            </ul>
-            <p>Schedule</p>
-            <ul>
-              <li>
-                <p>Mon-Fr</p>
-                <p>09:00 - 20:00</p>
-              </li>
-              <li>
-                <p>Sat</p>
-                <p>10:00 - 16:00</p>
-              </li>
-              <li>
-                <p>Sun</p>
-                <p>10:00 - 16:00</p>
-              </li>
-            </ul>
+            <div>
+              <Title>Call us</Title>
+              <PhoneContainer>
+                <li>
+                  <Link to="tel:0800222222">0 (800) 22-22-22</Link>
+                  <span>Free calls</span>
+                </li>
+                <li>
+                  <Link to="tel:+380673333333">+380 (67) 333-33-33</Link>
+                  <span>Kyiv</span>
+                </li>
+                <li>
+                  <Link to="tel:+380503333333">+380 (50) 333-33-33</Link>
+                  <span>Lviv</span>
+                </li>
+              </PhoneContainer>
+            </div>
+            <div>
+              <Title>Write us</Title>
+              <ul>
+                <li>
+                  <Link to="https://www.viber.com/ua/">Viber</Link>
+                </li>
+                <li>
+                  <Link to="https://web.telegram.org/">Telegram</Link>
+                </li>
+                <li>
+                  <Link to="mailto:sweet_paradise@mail.com">
+                    sweet_paradise@mail.com
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <Title>Schedule</Title>
+              <ul>
+                <li>
+                  <p>Mon-Fr</p>
+                  <p>09:00 - 20:00</p>
+                </li>
+                <li>
+                  <p>Sat</p>
+                  <p>10:00 - 16:00</p>
+                </li>
+                <li>
+                  <p>Sun</p>
+                  <p>10:00 - 16:00</p>
+                </li>
+              </ul>
+            </div>
           </Wrapper>
           <div>
             <h3>Our address</h3>
             <p>Maidan of Independence Kyiv, 02000</p>
-                  </div>
-                  <div>
-                      <div></div>
-                      <div></div>
-                  </div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
         </Container>
       </Section>
     </>
