c3b5ab7ff6107195be332e00e8b3c2c3bb8f301f
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-16 16:56:49 +0300

    save

--- src/redux/product/slice.tsx
+++ src/redux/product/slice.tsx
@@ -1,11 +1,27 @@
-const FETCH_PRODUCTS = "FETCH_PRODUCTS";
-const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
-const FETCH_PRODUCTS_ERROR = "FETCH_PRODUCTS_ERROR";
-
 interface ProductState {
   products: any[];
   loading: boolean;
   error: null | string;
+}
+
+enum ProductActionsTypes {
+    FETCH_PRODUCTS = 'FETCH_PRODUCTS',
+    FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS',
+    FETCH_PRODUCTS_ERROR = 'FETCH_PRODUCTS_ERROR',
+}
+
+interface FetchProductsAction {
+    type: ProductActionsTypes.FETCH_PRODUCTS;
+}
+
+interface FetchProductsSuccessAction {
+    type: ProductActionsTypes.FETCH_PRODUCTS_SUCCESS;
+    payload: any[];
+}
+
+interface FetchProductsErrorAction {
+    type: ProductActionsTypes.FETCH_PRODUCTS_ERROR ;
+    payload: string;
 }
 
 interface ProductAction {
@@ -24,11 +40,11 @@ export const productReducer = (
   action: ProductAction
 ): ProductState => {
   switch (action.type) {
-    case FETCH_PRODUCTS:
+    case ProductActionsTypes.FETCH_PRODUCTS:
       return { loading: true, error: null, products: [] };
-    case FETCH_PRODUCTS_SUCCESS:
+    case ProductActionsTypes.FETCH_PRODUCTS_SUCCESS:
       return { loading: false, error: null, products: action.payload };
-    case FETCH_PRODUCTS_ERROR:
+    case ProductActionsTypes.FETCH_PRODUCTS_ERROR:
       return { loading: false, error: action.payload, products: [] };
     default:
       return state;
