f054effbcbc8af85e9a631f20c71aada307fc21b
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-16 19:58:45 +0300

    add

--- src/redux/product/slice.jsx
+++ src/redux/product/slice.jsx
@@ -1,4 +1,4 @@
-// import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 // import { logOut } from "../auth/operations";
 // import {
 //   getProductsAll,
@@ -7,16 +7,46 @@
 //   deleteProduct,
 // } from "./operations";
 
+// interface ProductsType {
+//   _id: string;
+//   name: string;
+//   price: number;
+//   weight: number;
+//   category: string;
+//   favorite: boolean;
+//   img: File | null;
+//   ingredients: string;
+//   owner: string;
+// }
+
+// interface ProductsState {
+//   products: ProductsType[];
+//   isLoading: boolean;
+//   error: string | null;
+// }
+
+// const initialState: ProductsState = {
+//   products: [],
+//   isLoading: false,
+//   error: null,
+// };
+
+// const extraActions = [
+//   getProductsAll,
+//   getProductsByCategories,
+//   addProduct,
+//   deleteProduct,
+//   logOut,
+// ];
+
+// const getActions = (type: string) => extraActions.map((action) => action[type]);
+
 // const productsSlice = createSlice({
 //   name: "products",
-//   initialState: {
-//     items: [],
-//     isLoading: false,
-//     error: null,
-//   },
+//   initialState,
 //   reducers: {
 //     reset: (state) => {
-//       state.items = [];
+//       state.products = [];
 //       state.isLoading = false;
 //       state.error = null;
 //     },
@@ -24,38 +54,43 @@
 //   extraReducers: (builder) =>
 //     builder
 //       .addCase(getProductsAll.fulfilled, (state, action) => {
-//         state.items = action.payload;
+//         state.products = action.payload;
+//         state.isLoading = false;
+//         state.error = null;
+//       })
+//       .addCase(getProductsByCategories.fulfilled, (state, action) => {
+//         state.products = action.payload;
 //         state.isLoading = false;
 //         state.error = null;
 //       })
 //       .addCase(addProduct.fulfilled, (state, action) => {
-//         state.items.push(action.payload);
+//         state.products.push(action.payload);
 //         state.isLoading = false;
 //         state.error = null;
 //       })
 //       .addCase(deleteProduct.fulfilled, (state, action) => {
-//         const index = state.items.findIndex(
+//         const index = state.products.findIndex(
 //           (item) => item._id === action.payload.id
 //         );
-//         state.items.splice(index, 1);
+//         state.products.splice(index, 1);
 //         state.isLoading = false;
 //         state.error = null;
 //       })
 //       .addCase(logOut.fulfilled, (state) => {
-//         state.items = [];
+//         state.products = [];
 //         state.error = null;
 //         state.isLoading = false;
 //       })
-//       .addMatcher(
-//         (action) =>
-//           action.type.endsWith("/pending") || action.type.endsWith("/rejected"),
-//         (state) => {
-//           state.isLoading = true;
-//         }
-//       )
-//       .addMatcher(action.type.endsWith("/rejected"), (state, action) => {
+//       .addMatcher(isAnyOf(...getActions('pending')), state => {
+//         state.isLoading = true;
+//       })
+//       .addMatcher(isAnyOf(...getActions('rejected')), (state, action) => {
 //         state.isLoading = false;
 //         state.error = action.payload;
+//       })
+//       .addMatcher(isAnyOf(...getActions('fulfilled')), state => {
+//         state.isLoading = false;
+//         state.error = null;
 //       }),
 // });
 
