16e6b091563a25b7adcd22a84af2b315499f250d
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-16 12:07:21 +0300

    add sprite

--- src/layout/Contacts/Contacts.styled.tsx
+++ src/layout/Contacts/Contacts.styled.tsx
@@ -1,21 +1,76 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
-    margin-top: 20px;
-    display: flex;
-    column-gap: 20px;
-    margin-bottom: 20px;
-`
+  margin-top: 20px;
+  display: flex;
+  column-gap: 20px;
+  margin-bottom: 20px;
+`;
 
 export const MainTitle = styled.h2`
-    margin-bottom: 20px;
-`
+  margin-bottom: 20px;
+`;
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  color: #444251;
+  font-family: Open Sans;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+export const Text = styled.p`
+  color: #444251;
+  font-family: Open Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
 export const Title = styled.h3`
-    margin-bottom: 20px;
-`
+  color: #9c0746;
+  font-family: Montserrat;
+  font-size: 26px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-bottom: 20px;
+`;
+
 export const PhoneContainer = styled.ul`
-    display: grid;
-    grid-template-columns: [first] 220px [line2] 220px;
-    row-gap: 20px;
-`
+  display: grid;
+  grid-template-columns: [first] 240px [line2] 240px;
+  row-gap: 20px;
+`;
+
+export const SocialContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+export const ScheduleContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+`;
+
+export const AddressContainer = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const City = styled.span`
+  color: #444251;
+  font-family: Open Sans;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+export const IconContainer = styled.svg`
+  margin-right: 5px;
+`;
--- src/layout/Contacts/Contacts.tsx
+++ src/layout/Contacts/Contacts.tsx
@@ -1,7 +1,18 @@
-import { Link } from "react-router-dom";
 import { Container } from "../../components/Container/Container";
 import { Section } from "../../components/Section/Section";
-import { MainTitle, Wrapper, Title, PhoneContainer } from "./Contacts.styled";
+import icons from "../../images/sprite.svg";
+import {
+  MainTitle,
+  Wrapper,
+  Title,
+  PhoneContainer,
+  AddressContainer,
+  ScheduleContainer,
+  City,
+  IconContainer,
+  StyledLink,
+  Text,
+} from "./Contacts.styled";
 
 export const Contacts = () => {
   return (
@@ -14,16 +25,42 @@ export const Contacts = () => {
               <Title>Call us</Title>
               <PhoneContainer>
                 <li>
-                  <Link to="tel:0800222222">0 (800) 22-22-22</Link>
-                  <span>Free calls</span>
+                  <StyledLink to="tel:0800222222">
+                    {" "}
+                    <span>
+                      {" "}
+                      <IconContainer width={17} height={17}>
+                        <use href={icons + "#icon-phone"}></use>
+                      </IconContainer>
+                    </span>
+                    0 (800) 22-22-22
+                  </StyledLink>
+                  <City>Free calls</City>
                 </li>
                 <li>
-                  <Link to="tel:+380673333333">+380 (67) 333-33-33</Link>
-                  <span>Kyiv</span>
+                  <StyledLink to="tel:+380673333333">
+                    {" "}
+                    <span>
+                      {" "}
+                      <IconContainer width={17} height={17}>
+                        <use href={icons + "#icon-phone"}></use>
+                      </IconContainer>
+                    </span>
+                    +380 (67) 333-33-33
+                  </StyledLink>
+                  <City>Kyiv</City>
                 </li>
                 <li>
-                  <Link to="tel:+380503333333">+380 (50) 333-33-33</Link>
-                  <span>Lviv</span>
+                  <StyledLink to="tel:+380503333333">
+                    <span>
+                      {" "}
+                      <IconContainer width={17} height={17}>
+                        <use href={icons + "#icon-phone"}></use>
+                      </IconContainer>
+                    </span>
+                    +380 (50) 333-33-33
+                  </StyledLink>
+                  <City>Lviv</City>
                 </li>
               </PhoneContainer>
             </div>
@@ -31,45 +68,55 @@ export const Contacts = () => {
               <Title>Write us</Title>
               <ul>
                 <li>
-                  <Link to="https://www.viber.com/ua/">Viber</Link>
+                  <StyledLink to="https://www.viber.com/ua/">
+                    {" "}
+                    <IconContainer width={17} height={17}>
+                      <use href={icons + "#icon-viber"}></use>
+                    </IconContainer>
+                    Viber
+                  </StyledLink>
                 </li>
                 <li>
-                  <Link to="https://web.telegram.org/">Telegram</Link>
+                  <StyledLink to="https://web.telegram.org/">                      <IconContainer width={17} height={17}>
+                        <use href={icons + "#icon-telegram"}></use>
+                      </IconContainer>
+                    Telegram
+                  </StyledLink>
                 </li>
                 <li>
-                  <Link to="mailto:sweet_paradise@mail.com">
+                  <StyledLink to="mailto:sweet_paradise@mail.com">                      <IconContainer width={17} height={17}>
+                        <use href={icons + "#icon-mail"}></use>
+                      </IconContainer>
                     sweet_paradise@mail.com
-                  </Link>
+                  </StyledLink>
                 </li>
               </ul>
             </div>
             <div>
               <Title>Schedule</Title>
-              <ul>
-                <li>
-                  <p>Mon-Fr</p>
-                  <p>09:00 - 20:00</p>
-                </li>
-                <li>
-                  <p>Sat</p>
-                  <p>10:00 - 16:00</p>
-                </li>
-                <li>
-                  <p>Sun</p>
-                  <p>10:00 - 16:00</p>
-                </li>
-              </ul>
+              <ScheduleContainer>
+                <Text>Mon-Fr</Text>
+                <Text>09:00 - 20:00</Text>
+              </ScheduleContainer>
+              <ScheduleContainer>
+                <Text>Sat</Text>
+                <Text>10:00 - 16:00</Text>
+              </ScheduleContainer>
+              <ScheduleContainer>
+                <Text>Sun</Text>
+                <Text>10:00 - 16:00</Text>
+              </ScheduleContainer>
             </div>
           </Wrapper>
-          <div>
+          <AddressContainer>
             <Title>Our address</Title>
             <p>Maidan of Independence Kyiv, 02000</p>
-          </div>
+          </AddressContainer>
           <div>
             <div></div>
             <div>
               <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317.5632572258487!2d30.523549653589736!3d50.45030047805847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce50ffa1b91f%3A0x42d5962fe7e791c!2z0JzQsNC50LTQsNC9INCd0LXQt9Cw0LvQtdC20L3QvtGB0YLQuA!5e0!3m2!1sru!2sua!4v1697385917570!5m2!1sru!2sua"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d317.5632572258487!2d30.523549653589736!3d50.45030047805847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce50ffa1b91f%3A0x42d5962fe7e791c!2z0JzQsNC50LTQsNC9INCd0LXQt9Cw0LvQtdC20L3QvtGB0YLQuA!5e0!3m2!1sru!2sua!4v1697385917570!5m2!1sru!2sua"
                 width="600"
                 height="450"
                 style={{ border: 0 }}
