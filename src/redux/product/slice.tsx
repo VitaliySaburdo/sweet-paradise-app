b9fe2330c8d8195c906960e5cb14b73d6a6ee0ec
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-16 19:06:39 +0300

    add types

--- src/redux/product/slice.tsx
+++ src/redux/product/slice.tsx
@@ -1,33 +1,4 @@
-interface ProductState {
-  products: any[];
-  loading: boolean;
-  error: null | string;
-}
-
-enum ProductActionsTypes {
-    FETCH_PRODUCTS = 'FETCH_PRODUCTS',
-    FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS',
-    FETCH_PRODUCTS_ERROR = 'FETCH_PRODUCTS_ERROR',
-}
-
-interface FetchProductsAction {
-    type: ProductActionsTypes.FETCH_PRODUCTS;
-}
-
-interface FetchProductsSuccessAction {
-    type: ProductActionsTypes.FETCH_PRODUCTS_SUCCESS;
-    payload: any[];
-}
-
-interface FetchProductsErrorAction {
-    type: ProductActionsTypes.FETCH_PRODUCTS_ERROR ;
-    payload: string;
-}
-
-interface ProductAction {
-  type: string;
-  payload?: any;
-}
+import {ProductState, ProductAction, ProductActionsTypes } from '../../types/productsSlice';
 
 const initialState = {
   products: [],
