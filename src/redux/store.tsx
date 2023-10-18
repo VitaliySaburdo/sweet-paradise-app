c0dbb06a37d7ed4f689e2fecd293a398cf1a4e09
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-18 15:03:22 +0300

    hook

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -38,3 +38,4 @@ export const store = configureStore({
 export const persister = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
