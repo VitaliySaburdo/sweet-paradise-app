fb9b3885e6aaee67876a63039ca07d4ed6d4e2bf
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-19 10:07:10 +0300

    add novelties

---
+++ src/db/dataNovelties.tsx
@@ -0,0 +1,39 @@
+import Panna_cotta from "../images/Novelties/Panna_cotta.png";
+import Apple_tart from "../images/Novelties/Apple_tart.png";
+import Tiramisu from "../images/Novelties/Tiramisu.png";
+
+export const Novelties = [
+  {
+    _id: "ywiedgajhzvxzV12",
+    name: "Panna cotta",
+    price: 70,
+    weight: 200,
+    category: "novelties",
+    favorite: false,
+    img: { Panna_cotta },
+    ingredients:
+      "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
+    },
+      {
+    _id: "ywiedgajhzvxzV12",
+    name: "Tiramisu",
+    price: 80,
+    weight: 200,
+    category: "novelties",
+    favorite: false,
+    img: { Tiramisu },
+    ingredients:
+      "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
+    },
+        {
+    _id: "ywiedgajhzvxzV12",
+    name: "Apple tart",
+    price: 80,
+    weight: 200,
+    category: "novelties",
+    favorite: false,
+    img: { Apple_tart },
+    ingredients:
+      "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
+  },
+];
