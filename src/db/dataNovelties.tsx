26a4a2f02d172ece73054594108c1bb0d1c7ede6
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-19 10:15:29 +0300

    fix novelties

--- src/db/dataNovelties.tsx
+++ src/db/dataNovelties.tsx
@@ -1,39 +1,43 @@
 import Panna_cotta from "../images/Novelties/Panna_cotta.png";
 import Apple_tart from "../images/Novelties/Apple_tart.png";
 import Tiramisu from "../images/Novelties/Tiramisu.png";
+import { ProductProps } from "../App/App.types";
 
-export const Novelties = [
+export const Novelties: ProductProps[] = [
   {
-    _id: "ywiedgajhzvxzV12",
+    _id: "52e02dd5-0d8b-4492-b187-c875aa66a0c0",
     name: "Panna cotta",
     price: 70,
     weight: 200,
     category: "novelties",
     favorite: false,
-    img: { Panna_cotta },
+    img: Panna_cotta,
     ingredients:
       "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
-    },
-      {
-    _id: "ywiedgajhzvxzV12",
+    owner: 'Vitaliy',
+  },
+  {
+    _id: "04702f87-d4d3-451c-9107-c6bd8d8cf3dc",
     name: "Tiramisu",
     price: 80,
     weight: 200,
     category: "novelties",
     favorite: false,
-    img: { Tiramisu },
+    img: Tiramisu,
     ingredients:
       "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
-    },
-        {
-    _id: "ywiedgajhzvxzV12",
+    owner: 'Vitaliy',
+  },
+  {
+    _id: "33654b31-b4a2-4e40-abe4-ea213499f900",
     name: "Apple tart",
     price: 80,
     weight: 200,
     category: "novelties",
     favorite: false,
-    img: { Apple_tart },
+    img: Apple_tart,
     ingredients:
       "Fresh eggs, yolk, sugar, mascarpone, Savoyardi cookies, espresso, cognac, cocoa, salt",
+    owner: 'Vitaliy',
   },
 ];
