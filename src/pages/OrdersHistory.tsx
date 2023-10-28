9a230a56099614b85bdd9ffd6077e2b21dcd41e1
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-28 15:20:42 +0300

    add list

--- src/pages/OrdersHistory.tsx
+++ src/pages/OrdersHistory.tsx
@@ -1,13 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button/Button";
 import { Container } from "../components/Container/Container";
 import { Section } from "../components/Section/Section";
+import { useAppDispatch } from "../hooks/reduxHook";
+import { logOut } from "../redux/auth/authOperations";
 
 export const OrdersHistory = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   return (
     <>
       <Section>
         <Container>
           <h2>User history</h2>
         </Container>
+        <Button
+          onClick={() => {
+            dispatch(logOut);
+            navigate("/");
+          }}
+        >
+          Logout
+        </Button>
       </Section>
     </>
   );
