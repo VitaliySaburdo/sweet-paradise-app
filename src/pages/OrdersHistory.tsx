0caedbb6ffccfa0ebaa69d3b529bff75ccc73726
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-30 22:31:15 +0200

    add

--- src/pages/OrdersHistory.tsx
+++ src/pages/OrdersHistory.tsx
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/Button/Button";
 import { Container } from "../components/Container/Container";
 import { Section } from "../components/Section/Section";
-import { useAppDispatch } from "../hooks/reduxHook";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHook";
 import { logOut } from "../redux/auth/authOperations";
+import { getAllOrders } from "../redux/orders/ordersOperation";
+import { selectUser } from "../redux/auth/authSelectors";
+import { selectOrders } from "../redux/orders/ordersSelector";
 
 export const OrdersHistory = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const currentUser = useAppSelector(selectUser);
+  const orders = useAppSelector(selectOrders);
+
+  console.log(orders);
+
+  useEffect(() => {
+    dispatch(getAllOrders({ owner: currentUser._id }));
+  }, [dispatch, currentUser._id]);
 
   return (
     <>
