31395a34cb89e76e5ad18452859b0765e755b3b6
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-02 17:31:47 +0200

    delete / basename

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/sweet-paradise-app">
+      <BrowserRouter basename="sweet-paradise-app">
         <App />
         <ToastContainer />
       </BrowserRouter>
