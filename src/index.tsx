9e63fb903e82be25e930c8c58d2e45e2237252b8
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-02 17:14:22 +0200

    add /?/

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App/App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="sweet-paradise-app">
+      <BrowserRouter basename="/sweet-paradise-app">
         <App />
         <ToastContainer />
       </BrowserRouter>
