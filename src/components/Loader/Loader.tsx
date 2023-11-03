1727ef53709d3b29702b91c3c03f8530fe8ed54f
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-03 07:54:49 +0200

    <Loader/>

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,9 @@
+export const Loader = () => {
+  return (
+    <>
+      <div style={{ height: "800px" }}>
+        <p>Loading...</p>
+      </div>
+    </>
+  );
+};
--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -2,12 +2,13 @@ import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "../../layout/Header/Header";
 import { Footer } from "../../layout/Footer/Footer";
+import { Loader } from "../Loader/Loader";
 
 export const SharedLayout: React.FC = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={"Loading..."}>
+      <Suspense fallback={<Loader/>}>
         <main>
           <Outlet />
         </main>
