e888fd7748bc8055dd8bf8b071aac1829c08665c
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-06 08:23:17 +0200

    add dispatch goods

--- src/pages/Goods.tsx
+++ src/pages/Goods.tsx
@@ -1,38 +1,31 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { getProductsByCategories } from "../services/apiService";
+import { getProductsByCategories } from "../redux/product/productsOperations";
+import { useAppDispatch, useAppSelector } from "../hooks/reduxHook";
+import { selectIsLoading, selectProducts } from "../redux/product/productsSelectors";
 import { Catalog } from "../layout/Catalog/Catalog";
 import { Order } from "../components/Order/Order";
 
+
 const Goods: React.FC = () => {
   const [category, setCategory] = useState<string>("");
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+
+  const products = useAppSelector(selectProducts);
+  const loading = useAppSelector(selectIsLoading);
+
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        if (category) {
-          const data = await getProductsByCategories(category);
-          setProducts(data);
-          setLoading(false);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, [category]);
+    dispatch(getProductsByCategories(category));
+  }, [dispatch, category]);
 
   const handleChangeCategory = (id: string) => {
     setCategory(id);
   };
 
-  const changedCategory = async (id: string) => {
-    setCategory(id);
-    const data = await getProductsByCategories(category);
-    setProducts(data);
+  const changedCategory = async (category: string) => {
+    setCategory(category);
+    dispatch(getProductsByCategories(category));
   };
 
   return (
