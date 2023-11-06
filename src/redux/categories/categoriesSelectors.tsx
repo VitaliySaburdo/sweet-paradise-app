608f0f09cd5acc3f7e069f2f9dd565bd6dd9c3f6
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-06 19:49:12 +0200

    add

--- src/redux/categories/categoriesSelectors.tsx
+++ src/redux/categories/categoriesSelectors.tsx
@@ -4,3 +4,4 @@ export const selectCategory = (state: RootState) =>
   state.categories.currentCategory;
 export const selectCategories = (state: RootState) =>
   state.categories.categories;
+export const selectIsLoading = (state: RootState) => state.categories.isLoading;
