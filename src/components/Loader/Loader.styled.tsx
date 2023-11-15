e32483d01e29d3abe859a40e926eb392733356a9
VitaliySaburdo <vsaburdo@gmail.com>
2023-11-15 18:49:52 +0200

    500 Loader

--- src/components/Loader/Loader.styled.tsx
+++ src/components/Loader/Loader.styled.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  min-height: 700px;
+  min-height: 500px;
 `;
 
 export const Text = styled.p`
-    margin-top: 200px;
-    font-family: 'Sail',cursive;
-    color: #9C0746;
-    font-size: 22px;
-    text-align: center;
-`
+  margin-top: 200px;
+  font-family: "Sail", cursive;
+  color: #9c0746;
+  font-size: 22px;
+  text-align: center;
+`;
