acc6f7b609c95634ec293cdc849b29d99b052da0
VitaliySaburdo <vsaburdo@gmail.com>
2023-12-20 16:12:59 +0200

    fix icon on the form order

--- src/components/Icon/Icon.styled.tsx
+++ src/components/Icon/Icon.styled.tsx
@@ -4,8 +4,11 @@ export const SVG = styled.svg`
   display: block;
   position: absolute;
   left: 15px;
-  top: 12px;
+  top: 21px;
   fill: #555555;
   stroke: #555555;
   cursor: pointer;
+  @media screen and (${(props) => props.theme.media.md}) {
+    top: 12px;
+  }
 `;
